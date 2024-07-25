9925177b34b9f45312473d53fc57e2f2c80cbd67
phgusmao <pedro.gusmao1998@hotmail.com>
2024-07-25 13:30:36 -0300

    feature: finalizando backend

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,49 @@ app.post('/usuarios', async (req, res) => {
 
 app.get('/usuarios', async (req, res) => {
 
-    const users = await prisma.user.findMany()
+    let users = []
+
+    if(req.query) {
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                age: req.query.age,
+                email: req.query.email
+            }
+        })
+    } else {
+        await prisma.user.findMany()
+    }
 
     res.status(200).json(users)
+})
+
+app.put('/usuarios/:id', async (req, res) => {
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(201).json({message: 'Usuário deletado com sucesso.'})
+
 })
 
 app.listen(3000)
