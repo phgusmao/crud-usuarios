ba4f32526e13d933694ee24650105992ec0bb515
phgusmao <pedro.gusmao1998@hotmail.com>
2024-07-25 12:32:40 -0300

    feature: create and listen users

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client'
+import express from 'express'
+
+const prisma = new PrismaClient()
+
+const app = express()
+
+app.use(express.json())
+
+app.post('/usuarios', async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+
+})
+
+app.get('/usuarios', async (req, res) => {
+
+    const users = await prisma.user.findMany()
+
+    res.status(200).json(users)
+})
+
+app.listen(3000)
+
